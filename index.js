b8aac6396e06af954f90db564171414185461241
Kshitij Mohanka <kmohanka16@gmail.com>
2020-07-11 16:31:59 +0530

    let ,file size and result display changes

--- index.js
+++ index.js
@@ -2,22 +2,21 @@
 // Math.pow(length, 2) * Math.pow((frequency - 1), 0.33)
 // length: subsequence length
 // frequency: number of times the subsequence is repeated in the full file
-//  basically, you will have to call a random function that will generate a lowercase string(ASCII maar lena)
-//  before generating check if the size of the file jisme dump kara rahe ho , 3 gb hogya kya
+
 
 
 const fs = require('fs').promises;
 function randomString()
 {
-	var chars = "abcdefghiklmnopqrstuvwxyz";
-	var string_length = 300;		//any random values ,only for adding alphabets in the csv file
-	var j = 10;					//any random value accordingly for file size,but characters in different line
-	var randomstring = '';
+	let chars = "abcdefghiklmnopqrstuvwxyz";
+	let string_length = 1000;		//any random values ,only for adding alphabets in the csv file
+	let j = 100;					//any random value accordingly for file size,but characters in different line
+	let randomstring = '';
 	while (j > 0)
 	{
-		for (var i=0; i < string_length; i++) 
+		for (let i=0; i < string_length; i++) 
 		{
-			var rnum = Math.floor(Math.random() * chars.length);
+			let rnum = Math.floor(Math.random() * chars.length);
 			randomstring += chars.substring(rnum,rnum+1);
 		}
 		randomstring.split('\n')
@@ -26,8 +25,8 @@ function randomString()
 	return randomstring;
 }
 
-const f_name = 'file1.csv'
-const data = randomString(f_name);
+const f_name = "file1.csv";
+const data = randomString('file1.csv');
 function csv_file_size() 
 {
 	const fs = require("fs"); //Load the filesystem module
@@ -39,22 +38,22 @@ function csv_file_size()
 
 function bytesToSize(bytes) 
 {
-   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+   let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
    if (bytes == 0) return '0 Byte';
-   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+   let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 }
 
 
 function appending_csv()
 {
-	var fs = require('fs');
- 	var data1 = randomString();
+	let fs = require('fs');
+ 	let data1 = randomString();
 	fs.appendFileSync(f_name,data, 'utf8');    // callback function
 }
 
 
-async function main()
+async function main( f_size)
 {
 
 	try
@@ -65,10 +64,10 @@ async function main()
 	{
 	  console.log(e)
 	}
-	var loop = true
+	let loop = true
 	while(loop)
 	{
-		if (csv_file_size() < 32223443)
+		if (csv_file_size() < f_size)
 		{
 			appending_csv();
 		}
@@ -81,25 +80,23 @@ async function main()
 	try
 	{	
 	 	const data = await fs.readFile(f_name);
-	 	var h = {};
-	 	var hs = new Set();
-	 	var j = 0;
-	 	var i = 0;
+	 	let h = {};
+	 	let hs = new Set();
+	 	let j = 0;
+	 	let i = 0;
 		function hmm()
 		{
-	 		var counter = 1;
+	 		let counter = 1;
 	 		return counter
 	 	}
-	 	while(j < 1000)
+	 	while(j < 100000)	//loop must run till end of file to read all characters
 	 	{
-			var sub = '';
-			for( ;i < j + 3000; i++)		//iteration logic for 3000 characters
+			let sub = '';
+			for(let i = j;i < j + 30; i++)		//iteration logic for 3000 subsequence characters,change the value from 30 to 3000
 			{			
 				sub += String.fromCharCode(data[i]);
-			}
-			j += 1;
-			i = j 
-			
+			} 
+			j += 1
 			if (hs.has(sub) == true)
 			{
 				h[sub] += 1;
@@ -110,14 +107,22 @@ async function main()
 				h[sub] = hmm()
 			}
 		}
-		var max = Object.keys(h).reduce((a, v) => Math.max(a, h[v]), -Infinity);
-		var result = Object.keys(h).filter(v => h[v] === max);
+		// let max = Object.keys(h).reduce((a, v) => Math.max(a, h[v]), -Infinity);
+		// let result = Object.keys(h).filter(v => h[v] === max);
 		// console.log(result);		
-		for (const value of result) 
-		{
-	  		console.log(Math.pow(3000, 2) * Math.pow((h[value] - 1), 0.33));	//score of the subsequence
-	  		break;	//for displaying the score only once,for loop can be 
-		}
+		// for (const value of result) 
+		// {
+
+	 //  		console.log(Math.pow(3000, 2) * Math.pow((h[value] - 1), 0.33));	//score of the subsequence
+	 //  		break;	//for displaying the score only once,for loop can be 
+		// }
+		var max="";
+		var result = Object.keys(h).reduce(function(acc, val){
+		    if(max < h[val]) (max=h[val], acc={});
+		    if(h[val]==max) acc[val] = Math.pow(3000, 2) * Math.pow((h[val] - 1), 0.33);
+		    return acc;
+		},{});
+		console.log(result)		//will print the subsequence of 3000 characters and the score of that particular subsequence
 	}
 	catch (error)
 	{
@@ -125,6 +130,6 @@ async function main()
 	}
 }
 
-
-main()
+var desired_f_size = 3221225472;		//file size in bytes for creating a 3gb csv file 
+main(desired_f_size)
 
